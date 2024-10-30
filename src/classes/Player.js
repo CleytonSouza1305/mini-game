dfe50bf5806f7b2fce61ee39aa2dcfea97e0281a
Cleyton de Jesus Conceição Souza <141965244+CleytonSouza1305@users.noreply.github.com>
2024-10-30 16:07:26 -0300

    Adicionando a classe Player e teclas de movimento ao player

--- src/classes/Player.js
+++ src/classes/Player.js
@@ -1,11 +1,20 @@
 export default class Player {
 
-  constructor() {
+  constructor(canvasWidth, canvasHeight) {
     this.width = 100
     this.height = 100
     this.position = {
-      x: 0,
-      y: 0
+      x: canvasWidth / 2 - this.width / 2,
+      y: canvasHeight - this.height - 30
     }
+  }
+
+  draw(ctx) {
+    ctx.fillStyle = 'red'
+    ctx.fillRect(
+      this.position.x, 
+      this.position.y, 
+      this.width, 
+      this.height)
   }
 }
--- src/index.js
+++ src/index.js
@@ -6,7 +6,85 @@ const ctx = canvas.getContext("2d")
 canvas.width = innerWidth
 canvas.height = innerHeight
 
-const player = new Player()
+const player = new Player(canvas.width, canvas.height)
 
-ctx.fillStyle = 'red'
-ctx.fillRect(player.position.x, player.position.y, player.width, player.height)
+const keys = {
+  left: false,
+  right: false,
+  top: false,
+  bottom: false
+}
+
+const gameLoop = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  if (keys.left) {
+    player.position.x -= 6;
+  }
+
+  if (keys.right) {
+    player.position.x += 6;
+  }
+
+  if (keys.top) {
+    player.position.y -= 6;
+  }
+
+  if (keys.bottom) {
+    player.position.y += 6;
+  }
+
+  if (player.position.y < 30) {
+    player.position.y = 30;
+  }
+
+  if (player.position.y > canvas.height - player.height - 30) {
+    player.position.y = canvas.height - player.height - 30;
+  }
+
+  player.draw(ctx);
+
+  requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
+
+addEventListener('keydown', (ev) => {
+  const key = ev.key.toLowerCase();
+
+  if (key === 'a') {
+    keys.left = true;
+  }
+
+  if (key === 'd') {
+    keys.right = true;
+  }
+
+  if (key === 'w') {
+    keys.top = true;
+  }
+  
+  if (key === 's') {
+    keys.bottom = true;
+  }
+});
+
+addEventListener('keyup', (ev) => {
+  const key = ev.key.toLowerCase();
+
+  if (key === 'a') {
+    keys.left = false;
+  }
+
+  if (key === 'd') {
+    keys.right = false;
+  }
+
+  if (key === 'w') {
+    keys.top = false;
+  }
+  
+  if (key === 's') {
+    keys.bottom = false;
+  }
+});
