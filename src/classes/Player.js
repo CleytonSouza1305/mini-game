04b6a06323ebf47c2f1a6252362bd5b29b3e783f
Cleyton de Jesus Conceição Souza <141965244+CleytonSouza1305@users.noreply.github.com>
2024-11-01 19:55:16 -0300

    Adicionando componentes do foguete

--- src/classes/Player.js
+++ src/classes/Player.js
@@ -1,16 +1,18 @@
-import { PATH_SPACESHIP_IMAGE } from "../utils/constantes.js"
+import { PATH_ENGINE_IMAGE, PATH_ENGINE_SPRITES, PATH_SPACESHIP_IMAGE } from "../utils/constantes.js"
 
 export default class Player {
 
   constructor(canvasWidth, canvasHeight) {
-    this.width = 48 * 2
-    this.height = 48 * 2
+    this.width = 48 * 4
+    this.height = 48 * 4
     this.velocity = 6
     this.position = {
       x: canvasWidth / 2 - this.width / 2,
       y: canvasHeight - this.height - 30
     }
     this.image = this.getImage(PATH_SPACESHIP_IMAGE)
+    this.engineImage = this.getImage(PATH_ENGINE_IMAGE)
+    this.engineSprite = this.getImage(PATH_ENGINE_SPRITES)
   }
 
   getImage(path) {
@@ -37,5 +39,17 @@ export default class Player {
 
   draw(ctx) {
     ctx.drawImage(this.image, this.position.x, this.position.y, this.width, this.height)
+
+    ctx.drawImage(this.engineSprite, 
+      0, 
+      0, 
+      48, 
+      48, 
+      this.position.x, 
+      this.position.y, 
+      this.width, 
+      this.height)
+
+    ctx.drawImage(this.engineImage, this.position.x, this.position.y + 8, this.width, this.height)
   }
 }
